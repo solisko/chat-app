979b3b4d946054c1626a80f1108f4c227e48beb4
Sofia Skommar <sofiaskommar@gmail.com>
2024-08-07 20:05:02 +0200

    logout chat

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import { ChatContext } from "../Context/ChatContextProvider";
+import { useNavigate } from "react-router-dom";
+
 const Chat = () => {
+  const { logout } = useContext(ChatContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <div>
-        <h1>Chat</h1>
+      <h1>Chat</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
-}
-export default Chat
+  );
+};
+export default Chat;
