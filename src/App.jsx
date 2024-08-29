a16767fc74d6440ee73048235ad99fcc07c4b967
Sofia Skommar <sofiaskommar@gmail.com>
2024-08-29 16:05:09 +0200

    staring over

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,6 @@ import Register from "./Components/Register";
 import Chat from "./Components/ChatRoom/Chat";
 import Profile from "./Components/Profile";
 import ShowSendMsg from "./Components/ChatRoom/ShowSendMsg";
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-const container = document.getElementById(“app”);
-const root = createRoot(container);
-root.render(<App />);
 
 function App() {
   return (
@@ -54,7 +34,6 @@ function App() {
           />
         </Router>
       </ChatContextProvider>
-      {/* <button style={{ border: '5px solid white' }} onClick={() => methodDoesNotExist()}>Break the world</button> */}
     </div>
   );
 }
